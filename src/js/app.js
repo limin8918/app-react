5730c50fae254a54ffda9b556478fb44c1aad4da
Li Min <li.min@rea-group.com>
2016-08-08 17:59:33 +0800

    add public path

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,8 @@
 require('babel-register' );
 require('./server');
+var express = require('express'),
+    app = express();
+
+app.use('/public', express.static('./public'));
+
+app.listen(process.env.PORT);
